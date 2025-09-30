74dda3509ff26808ba2bf9927c3776a50abe91fb
qasim <qasimmizbah@gmail.com>
2025-09-30 17:53:56 +0530

    code update

--- services/login_service.py
+++ services/login_service.py
@@ -3,6 +3,7 @@ from fastapi import HTTPException, Request
 from utils.security import verify_password, create_access_token
 from services.logs_service import write_log
 
+
 async def user_login_service(user, request: Request):
     async with request.app.state.pool.acquire() as conn:
         db_user = await conn.fetchrow("SELECT * FROM app_user WHERE is_active='true' and email=$1", user.email)
@@ -19,14 +20,9 @@ async def user_login_service(user, request: Request):
             )
             raise HTTPException(status_code=401, detail="Invalid credentials")
 
-        # token_expires = timedelta(minutes=3600)
-        # access_token = create_access_token(
-        #     data={"user_id": str(db_user["id"]),"role": db_user["role"]}, expires_delta=token_expires
-        # )
         
         
         return {
-            
             "user": {
                 "id": db_user["id"],
                 "email": db_user["email"],
