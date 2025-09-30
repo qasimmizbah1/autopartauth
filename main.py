b27a8ae4a2cd3fa398fe8fb69bd2c6b14da1e2e6
qasim <qasimmizbah@gmail.com>
2025-09-30 12:47:50 +0530

    Update main file

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from database import lifespan
 from routers import auth
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+import os
+import uvicorn
 
 app = FastAPI(lifespan=lifespan)
 
@@ -19,6 +22,12 @@ app.include_router(auth.router)
 
 
 @app.get("/")
+@app.head("/")
 async def read_root():
     return {"message": "Welcome to the Auto Parts auth API "} 
     
+
+# Run with Render's assigned port
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # default 8000 for local testing
+    uvicorn.run(app, host="0.0.0.0", port=port)
